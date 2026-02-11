d3b111e7381b272cf770651144e70c34b04ed2e2
Valeria Berdnikova <valber160707@gmail.com>
2026-02-11 17:20:27 +0500

    task about checking array sorting

---
+++ sorted_array_or_no/sorted_array_or_no.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "ru");
+    int arr[50];
+    int n;
+    bool f = true;
+    cout << "Введите размер массива: ";
+    cin >> n;
+    if (n <= 0 || n >= 51) {
+        while (n <= 0 || n >= 51) {
+            cout << "Введите другой размер (0<n<51): ";
+            cin >> n;
+        }
+    }
+    cout << "Получившийся массив: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+
+    for (int i = 1; i < n && f == true; i++) {
+        if (arr[i - 1] >= arr[i]) {
+            f = false;
+        }
+    }
+
+    if (f == false)
+        cout << "Массив не упорядочен";
+    else
+        cout << "Массив упорядочен" << endl;
+    return 0;
+}
