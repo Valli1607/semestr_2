7af9884c4f728c2cb2397afa4da9c072980f81af
Valeria Berdnikova <valber160707@gmail.com>
2026-02-11 18:19:15 +0500

    task about sorted by insertion and sorted with bool f

---
+++ sorted_by_insertion/sorted_by_insertion.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "ru");
+    int n;
+    cout << "Введите размер массива: ";
+    cin >> n;
+    int arr[50];
+    cout << "Исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+    }
+    cout << "Получившийся массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
---
+++ sorted_by_insertion/sorted_by_insertion1.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main(){
+    const int n = 50;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 1; i < N; ++i) {
+        int t = arr[i];
+        int j = i - 1;
+        bool f = false;
+        while (j >= 0 && !f) {
+            if (arr[j] > t) {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            else {
+                f = true;
+            }
+        }
+        arr[j + 1] = t;
+    }
+    for (int i = 0; i < n; i++)
+        cout << arr[i];
+}
