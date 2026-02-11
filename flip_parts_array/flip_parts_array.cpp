a89c201b9a10b13b16f069b1c988c2086d462333
Valeria Berdnikova <valber160707@gmail.com>
2026-02-11 17:26:15 +0500

    add task about flip parts array

---
+++ flip_parts_array/flip_parts_array.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int A[100];
+    int n, d, p, q;
+    cin >> n >> p >> q;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    int i = p;
+    int j = q;
+    while (i < j) {
+        d = A[i];
+        A[i] = A[j];
+        A[j] = d;
+        i++;
+        j--;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    return 0;
+}
